86b979f294ed92978d1457310aa836c2a4da09ff
Mateus Alves <134669543+MateusDev6@users.noreply.github.com>
2023-10-07 13:17:43 -0300

    add active column
    feat : add active column

--- src/main/java/com/example/crud/controlers/productController.java
+++ src/main/java/com/example/crud/controlers/productController.java
@@ -3,10 +3,13 @@ package com.example.crud.controlers;
 import com.example.crud.domain.product.Product;
 import com.example.crud.domain.product.ProductRepository;
 import com.example.crud.domain.product.RequestProductDTO;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 
 @SuppressWarnings("ALL")
@@ -18,7 +21,7 @@ public class productController {
 
     @GetMapping
     public ResponseEntity getAllProducts() {
-        var allProducts = repository.findAll();
+        var allProducts = repository.findAllByActiveTrue();
 
         return ResponseEntity.ok(allProducts);
     }
@@ -30,11 +33,29 @@ public class productController {
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/id")
+    @PutMapping()
+    @Transactional
     public ResponseEntity updateProduct(@RequestBody @Valid RequestProductDTO data) {
-        Product product = repository.getReferenceById(data.id());
+        Optional<Product> optionalProduct = repository.findById(data.id());
+        if (optionalProduct.isPresent()){
+            Product product=optionalProduct.get();
+            product.setName(data.name());
+            product.setPrice_in_cents(data.price_in_cents());
+            return ResponseEntity.ok(optionalProduct);
+        }
+        return ResponseEntity.notFound().build();
+    }
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity deleteProduct(@PathVariable String id) {
+        Optional<Product> optionalProduct = repository.findById(id);
+        if (optionalProduct.isPresent()){
+            Product product=optionalProduct.get();
+            product.setActive(false);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
 
-
-        return ResponseEntity.ok(product);
     }
 }
+
